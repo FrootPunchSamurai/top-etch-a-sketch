8e5bae8d809bad082f030229edd3f7ed5519664f
Amruth Kiran <amruthkiran47@gmail.com>
2022-01-21 17:06:17 +0530

    mouseover feature added

--- script.js
+++ script.js
@@ -1,14 +1,32 @@
 const grid = document.getElementById('grid');
+const slider = document.getElementById('myRange');
+
+slider.oninput = function(){
+    initGrid();
+};
+
+function initGrid(){
+    let val = parseInt(slider.value);
+    grid.innerHTML = '';
+    createGrid(val);
+}
 
 function createGrid(size){
+
     grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
     for(let i=0; i<size*size; i++){
         const gridElement = document.createElement('div');
+        gridElement.addEventListener('mouseover', changeColor);
         gridElement.classList.add('grid-item');
         grid.appendChild(gridElement);
     }
 }
 
-createGrid(16);
+function changeColor(e){
+    const target = e.target;
+    target.classList.add('black');
+}
+
+
